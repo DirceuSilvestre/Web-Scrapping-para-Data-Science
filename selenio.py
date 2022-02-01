870b42b719cab3b153eefcf3d756273e038e54f4
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2022-02-01 17:09:52 -0300

    funcionando ate listar pelo menor preço

--- selenio.py
+++ selenio.py
@@ -1,6 +1,13 @@
 from time import sleep
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from unicodedata import name
+from matplotlib.pyplot import table
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+
 ########### Acima #################
 #Importa as bibliotecas necessárias
 
@@ -29,12 +36,36 @@ drive.maximize_window()
 #e clica nele para que busque o termo escrito anteriormente
 #após aguardar o carregamento dos resultados fecha o navegador, encerrando o processo
 
+sleep(2)
 busca = drive.find_element_by_tag_name('input')
 busca.click()
+
+
 busca.send_keys('lápis hb')
+
 busca = drive.find_element_by_tag_name('button')
+
 busca.click()
 
-sleep(3)
+menor_preco = drive.find_element_by_xpath("//form[@class='src__SortBar-sc-1oc11r-0 hEjDBT']")
+menor_preco.click()
+
+menor_preco = drive.find_element_by_xpath("//option[@value='lowerPrice']")
+menor_preco.click()
+
+produto = drive.find_element_by_xpath("//h3[@class='product-name__Name-sc-1shovj0-0 gUjFDF']")
+
+html_content = produto.get_attribute('outherHTML')
+
+
+soup = BeautifulSoup(html_content,'html.parser')
+table = soup.find(name=['table'])
+
+df_full = pd.read_html(str(table))[0].head(10)
+
+print(df_full)
+
+
+sleep(6)
 
 drive.quit()
