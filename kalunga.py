bbeb5ca31474d8844b15957b93ef71f238633a45
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2023-08-03 15:32:12 -0300

    agora vai, coleta 1 dia 03/08/2023

--- kalunga.py
+++ kalunga.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import pandas as pd
 
 # a lista de produtos que eu quero saber os preços no site da Kalunga
-produtos_buscados = ["caderno 10 materias", "lapis de cor 12 cores", "caneta bic azul", "lapis grafite hb", "estojo escolar", "borracha branca", "apontador", "regua 20cm", "tesoura sem ponta", "mochila escolar"]
+produtos_buscados = ["mochila escolar"]
 
 # ["caderno 10 materias", "lapis de cor 12 cores", "caneta bic azul", "lapis grafite hb", "estojo escolar", "borracha branca", "apontador", "regua 20cm", "tesoura sem ponta", "mochila escolar"]
 
